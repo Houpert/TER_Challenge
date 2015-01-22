a5e2cc4037bb9c620d29abb69aa9b8a8e9d5b5f9
yhoupert <yhoupert@linagora.com>
2015-01-22 14:49:25 +0100

    Add function to find the good trait to game

--- app/src/main/java/Controller/DotsAndBoxes.java
+++ app/src/main/java/Controller/DotsAndBoxes.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import android.app.ActivityOptions;
+
 import Domain.Action;
 import Domain.Trait;
 
@@ -63,5 +65,242 @@ public class DotsAndBoxes {
         for(int i=0; i<3; i++){
 
         }
+    }
+
+    public boolean addTrait(Action action, int point){
+        if(Action.EMPTY == action)
+            return false;
+
+        switch (point) {
+            case 1:
+                if (action == Action.UP || action == Action.LEFT)
+                    return false;
+                if (action == Action.DOWN) {
+                    if (plateau[0][1].isDraw())
+                        return false;
+                    else {
+                        plateau[0][1].setDraw(true);
+                        return true;
+                    }
+                }
+                if (action == Action.RIGHT){
+                    if (plateau[0][0].isDraw())
+                        return false;
+                    else {
+                        plateau[0][0].setDraw(true);
+                        return true;
+                    }
+                }
+                break;
+            case 2:
+                if(action == Action.UP)
+                    return false;
+                if(action == Action.LEFT){
+                    if (plateau[0][0].isDraw())
+                        return false;
+                    else {
+                        plateau[0][0].setDraw(true);
+                        return true;
+                    }
+                }
+                if(action == Action.RIGHT){
+                    if (plateau[1][0].isDraw())
+                        return false;
+                    else {
+                        plateau[1][0].setDraw(true);
+                        return true;
+                    }
+                }
+                if(action == Action.DOWN){
+                    if (plateau[0][2].isDraw())
+                        return false;
+                    else {
+                        plateau[0][2].setDraw(true);
+                        return true;
+                    }
+                }
+                break;
+            case 3:
+                if (action == Action.UP || action == Action.RIGHT)
+                    return false;
+                if (action == Action.DOWN) {
+                    if (plateau[1][2].isDraw())
+                        return false;
+                    else {
+                        plateau[1][2].setDraw(true);
+                        return true;
+                    }
+                }
+                if (action == Action.LEFT){
+                    if (plateau[1][0].isDraw())
+                        return false;
+                    else {
+                        plateau[1][0].setDraw(true);
+                        return true;
+                    }
+                }
+                break;
+            case 4:
+                if(action == Action.LEFT)
+                    return false;
+                if(action == Action.UP){
+                    if (plateau[0][1].isDraw())
+                        return false;
+                    else {
+                        plateau[0][1].setDraw(true);
+                        return true;
+                    }
+                }
+                if(action == Action.RIGHT){
+                    if (plateau[1][3].isDraw())
+                        return false;
+                    else {
+                        plateau[1][3].setDraw(true);
+                        return true;
+                    }
+                }
+                if(action == Action.DOWN){
+                    if (plateau[2][1].isDraw())
+                        return false;
+                    else {
+                        plateau[2][1].setDraw(true);
+                        return true;
+                    }
+                }
+                break;
+            case 5:
+                if(action == Action.LEFT)
+                    if (plateau[0][3].isDraw())
+                        return false;
+                    else {
+                        plateau[0][3].setDraw(true);
+                        return true;
+                    }
+                if(action == Action.UP){
+                    if (plateau[0][2].isDraw())
+                        return false;
+                    else {
+                        plateau[0][2].setDraw(true);
+                        return true;
+                    }
+                }
+                if(action == Action.RIGHT){
+                    if (plateau[1][3].isDraw())
+                        return false;
+                    else {
+                        plateau[1][3].setDraw(true);
+                        return true;
+                    }
+                }
+                if(action == Action.DOWN){
+                    if (plateau[2][2].isDraw())
+                        return false;
+                    else {
+                        plateau[2][2].setDraw(true);
+                        return true;
+                    }
+                }
+                break;
+            case 6:
+                if(action == Action.RIGHT)
+                    return false;
+                if(action == Action.UP){
+                    if (plateau[1][2].isDraw())
+                        return false;
+                    else {
+                        plateau[1][2].setDraw(true);
+                        return true;
+                    }
+                }
+                if(action == Action.LEFT){
+                    if (plateau[1][3].isDraw())
+                        return false;
+                    else {
+                        plateau[1][3].setDraw(true);
+                        return true;
+                    }
+                }
+                if(action == Action.DOWN){
+                    if (plateau[3][2].isDraw())
+                        return false;
+                    else {
+                        plateau[3][2].setDraw(true);
+                        return true;
+                    }
+                }
+                break;
+            case 7:
+                if (action == Action.DOWN || action == Action.LEFT)
+                    return false;
+                if (action == Action.UP) {
+                    if (plateau[2][1].isDraw())
+                        return false;
+                    else {
+                        plateau[2][1].setDraw(true);
+                        return true;
+                    }
+                }
+                if (action == Action.RIGHT){
+                    if (plateau[2][3].isDraw())
+                        return false;
+                    else {
+                        plateau[2][3].setDraw(true);
+                        return true;
+                    }
+                }
+                break;
+            case 8:
+                if(action == Action.DOWN)
+                    return false;
+                if(action == Action.UP){
+                    if (plateau[2][2].isDraw())
+                        return false;
+                    else {
+                        plateau[2][2].setDraw(true);
+                        return true;
+                    }
+                }
+                if(action == Action.LEFT){
+                    if (plateau[2][3].isDraw())
+                        return false;
+                    else {
+                        plateau[2][3].setDraw(true);
+                        return true;
+                    }
+                }
+                if(action == Action.RIGHT){
+                    if (plateau[3][3].isDraw())
+                        return false;
+                    else {
+                        plateau[3][3].setDraw(true);
+                        return true;
+                    }
+                }
+                break;
+            case 9:
+                if (action == Action.DOWN || action == Action.RIGHT)
+                    return false;
+                if (action == Action.UP) {
+                    if (plateau[3][2].isDraw())
+                        return false;
+                    else {
+                        plateau[2][2].setDraw(true);
+                        return true;
+                    }
+                }
+                if (action == Action.LEFT){
+                    if (plateau[3][3].isDraw())
+                        return false;
+                    else {
+                        plateau[3][3].setDraw(true);
+                        return true;
+                    }
+                }
+                break;
+            default:
+                return false;
+        }
+
+        return false;
     }
 }
