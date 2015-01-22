94236016a528203fa4dc71aae8921c4553a9a8f2
yhoupert <yhoupert@linagora.com>
2015-01-22 10:10:12 +0100

    add the touch screen and the screenOrientation

--- app/src/main/java/ter/android/ter_challenge/GameActivity.java
+++ app/src/main/java/ter/android/ter_challenge/GameActivity.java
@@ -2,18 +2,58 @@ package ter.android.ter_challenge;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import Domain.Trait;
 
 
 public class GameActivity extends ActionBarActivity {
+
+    private final int nbOddsSquare = 4;
+    private final int nbSquare = 4;
+
+    Trait[][] plateau = new Trait[nbSquare][nbOddsSquare];
+
+    private final String TAG = "Debug -- ";
+    private int height;
+    private int width;
+
+    private RelativeLayout gameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_layout);
-    }
 
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        height = displaymetrics.heightPixels;
+        width = displaymetrics.widthPixels;
+
+        initLogic();
+
+        gameLayout = (RelativeLayout) findViewById(R.id.gameLayout);
+
+
+        gameLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //gesture detector to detect swipe.
+
+
+                int touchX = (int) event.getX();
+                int touchY = (int) event.getY();
+
+                squareDetector(touchX, touchY);
+                return true;//always return true to consume event
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +76,39 @@ public class GameActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    private void initLogic() {
+        plateau[0][0]=new Trait(false);
+        plateau[0][1]=new Trait(false);
+        plateau[1][0]=new Trait(false);
+        plateau[1][2]=new Trait(false);
+        plateau[2][1]=new Trait(false);
+        plateau[2][3]=new Trait(false);
+        plateau[3][2]=new Trait(false);
+        plateau[3][3]=new Trait(false);
+
+        Trait  tH = new Trait(false);
+        Trait  tB = new Trait(false);
+        Trait  tD = new Trait(false);
+        Trait  tG = new Trait(false);
+
+        plateau[0][2]= tH;
+        plateau[1][1]= tH;
+        plateau[0][3]= tG;
+        plateau[2][0]= tG;
+        plateau[1][3]= tD;
+        plateau[3][0]= tD;
+        plateau[2][2]= tB;
+        plateau[3][1]= tB;
+
+    }
+
+
+    private void squareDetector(int touchX, int touchY) {
+
+
+
+    }
+
 }
